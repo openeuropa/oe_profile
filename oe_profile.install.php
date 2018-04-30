71c2fc710f77b94eb7c8938700d9741ac18b4a1b
richardcanoe <richardcanoe@gmail.com>
2018-04-30 14:31:23 +0200

    Profile install file added

---
+++ oe_profile.install.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Install, update and uninstall functions for the oe_profile install profile.
+ */
+
+/**
+ * Implements hook_install().
+ *
+ * Perform actions to set up the site for this profile.
+ *
+ * @see system_install()
+ */
+function oe_profile_install() {
+
+	// Can add code in here to make nodes, terms, etc.
+}
